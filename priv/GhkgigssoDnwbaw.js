ecac71105a72d6fb3b0c96047612cc1070aadc70
EdilAhtera <eadlalamsa@gmail.com>
2024-07-29 17:56:04 +0700

    GhkgigssoDnwbaw.js

--- priv/GhkgigssoDnwbaw.js
+++ priv/GhkgigssoDnwbaw.js
@@ -144,7 +144,7 @@ background-color: white;
                 <div class="card-side card-back"></div>
             </div>
         </div>
-        <p>You are holding the business card of<br> Mr M Aidil Alamsyah, CEO of Ahtera</p>
+        <p>Anda memegang kartu nama milik CEO Ahtera, Mr. M. Aidil Alamsyah</p>
         <div class="button-pair">
             <a href="#" class="action-button" id="cancel-button">Cancel</a>
             <a href="priv/father.html" class="action-button">View</a>
