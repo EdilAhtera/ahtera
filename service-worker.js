7480b2941b66a3428a5b6f9550d42014f4ff550b
EdilAhtera <eadlalamsa@gmail.com>
2024-07-31 18:20:42 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,22 @@
-const CACHE_NAME = 'ahtera-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    'edilahtera.github.io/ahtera/index.html',
-    '/script.js',
-    '/footer.html',
-    '/header.html',
-    '/contact-us.html',
-    '/b/about-us.html',
-    '/b/media-assets.html',
-    'edilahtera.github.io/ahtera/footer.html',
-    'edilahtera.github.io/ahtera/header.html',
-    'edilahtera.github.io/ahtera/contact-us.html',
-    'edilahtera.github.io/ahtera/b/about-us.html',
-    'edilahtera.github.io/ahtera/b/media-assets.html',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
-];
-
-self.addEventListener('install', event => {
+// service-worker.js
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            return cache.addAll(urlsToCache);
+        caches.open('my-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/main.js',
+                '/manifest.json'
+            ]);
         })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-        .then(response => {
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        })
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
         })
     );
 });
