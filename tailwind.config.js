259a45dfaf47fc9c200a6589b95c6bc4892221d6
Ahtera Studio <eadlalamsa@gmail.com>
2024-09-15 09:35:41 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,22 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-'info-gray': '#d3d3d3',
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+        'info-gray': '#d3d3d3',
+        primary: {
+          "50": "#eff6ff",
+          "100": "#dbeafe",
+          "200": "#bfdbfe",
+          "300": "#93c5fd",
+          "400": "#60a5fa",
+          "500": "#3b82f6",
+          "600": "#2563eb",
+          "700": "#1d4ed8",
+          "800": "#1e40af",
+          "900": "#1e3a8a",
+          "950": "#172554"
+        },
+        'custom-green': '#AFE507',
+        'custom-black': '#0e0e0e'
       }
     },
     fontFamily: {
